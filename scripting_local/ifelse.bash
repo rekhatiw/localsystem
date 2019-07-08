3fcc4e1ce90ed3c4b4cdbe3e51c6f6d4de28be87
Tiwari <rekha.tiwari@atos.net>
2019-07-08 14:49:07 +0530

    my first local commit

---
+++ scripting_local/ifelse.bash
@@ -0,0 +1,9 @@
+vi bash-ifelse.sh
+       #!/bin/bash
+
+       total=$0
+       if [ $total -eq 1 ]; then
+         echo "the number is equal to 1"
+       else
+         echo "the number is NOT equal to 1"
+       fi
